ad86e5b26e633115a04c890746f1e2c6be8974cc
Pablech <padagoec@gmail.com>
2025-12-16 21:59:04 -0300

    feat(days 1-2): add solutions, READMEs and auxiliary files

---
+++ day_1/day_1_historian_hysteria.py
@@ -0,0 +1,43 @@
+from collections import Counter
+
+
+def parte_1(esquerda: list[int], direita: list[int]) -> int:
+    esquerda = sorted(esquerda)
+    direita = sorted(direita)
+    dif = 0
+
+    for i in range(len(esquerda)):
+        l_1 = esquerda[i]
+        l_2 = direita[i]
+        dif += abs(l_1 - l_2)
+
+    return dif
+
+
+def parte_2(esquerda: set[int], direita: list[int]) -> int:
+    total = 0
+
+    contagem = Counter(direita)
+    for num in esquerda:
+        total += num * contagem.get(num, 0)
+
+    return total
+
+
+def main():
+    with open('IDs.txt', 'r', encoding='utf-8') as arquivo:
+        ids = arquivo.read()
+
+    lista_ids = ids.split()
+
+    esquerda, direita = [], []
+
+    for i in range(len(lista_ids)):
+        esquerda.append(int(lista_ids[i])) if i % 2 == 0 else direita.append(int(lista_ids[i]))
+
+    print(parte_1(esquerda, direita))
+    print(parte_2(set(esquerda), direita))
+
+
+if __name__ == '__main__':
+    main()
---
+++ day_2/day_2_Red-Nosed-Reports.py
@@ -0,0 +1,52 @@
+def avaliar(relatorio: list[int]) -> bool:
+    inverte = relatorio[0] > relatorio[-1]
+    dif_validas = {1, 2, 3}
+
+    for i in range(len(relatorio) - 1):
+        l, r = relatorio[i], relatorio[i + 1]
+
+        if inverte and l < r:
+            return False
+        if not inverte and l > r:
+            return False
+
+        dif = abs(l - r)
+
+        if dif not in dif_validas:
+            return False
+
+    return True
+
+
+def dampener(relatorio: list[int]) -> bool:
+    if avaliar(relatorio):
+        return True
+
+    for i in range(len(relatorio)):
+
+        temp_relatorio = relatorio.copy()
+        temp_relatorio.pop(i)
+
+        if avaliar(temp_relatorio):
+            return True
+
+    return False
+
+
+def main():
+    with open('relatorios.txt', 'r', encoding='utf-8') as arquivo:
+        relatorios = arquivo.read().split('\n')
+
+    seguros = 0
+
+    for relatorio_str in relatorios:
+        relatorio_int = [int(n) for n in relatorio_str.split(' ')]
+
+        if dampener(relatorio_int):
+            seguros += 1
+
+    print(f'{seguros = }')
+
+
+if __name__ == '__main__':
+    main()
