5136e6e3cd7d4f4308708d42795eecaa7272f4a9
Pablech <padagoec@gmail.com>
2025-12-18 21:21:29 -0300

    Optimization of the part_1 function

--- day_4/day_4_Ceres-Search.py
+++ day_4/day_4_Ceres-Search.py
@@ -1,44 +1,41 @@
-def part_1(arquivo: str) -> int:
-    cont_horizontal = 0
-    for i in range(len(arquivo)):
-        for j in range(len(arquivo[i]) - 3):
-            palavra = arquivo[i][j] + arquivo[i][j + 1] + arquivo[i][j + 2] + arquivo[i][j + 3]
-            if palavra == 'XMAS' or palavra == 'SAMX':
-                cont_horizontal += 1
+def part_1(arquivo: list[str]) -> int:
+    cont = 0
+    linhas = len(arquivo)
+    colunas = len(arquivo[0])
+    palavras = ['XMAS', 'SAMX']
 
-    cont_vertical = 0
-    for i in range(len(arquivo) - 3):
-        for j in range(len(arquivo[i])):
-            palavra = arquivo[i][j] + arquivo[i + 1][j] + arquivo[i + 2][j] + arquivo[i + 3][j]
-            if palavra == 'XMAS' or palavra == 'SAMX':
-                cont_vertical += 1
+    for i in range(linhas):
+        for j in range(colunas):
 
-    cont_diagonal_direita = 0
-    for i in range(len(arquivo) - 3):
-        for j in range(len(arquivo[i]) - 3):
-            palavra = arquivo[i][j] + arquivo[i + 1][j + 1] + arquivo[i + 2][j + 2] + arquivo[i + 3][j + 3]
-            if palavra == 'XMAS' or palavra == 'SAMX':
-                cont_diagonal_direita += 1
+            if j < colunas - 3:
+                teste = arquivo[i][j] + arquivo[i][j+1] + arquivo[i][j+2] + arquivo[i][j+3]
+                cont += 1 if teste in palavras else 0
 
-    cont_diagonal_esquerda = 0
-    for i in range(len(arquivo) - 3):
-        for j in range(3, len(arquivo[i])):
-            palavra = arquivo[i][j] + arquivo[i + 1][j - 1] + arquivo[i + 2][j - 2] + arquivo[i + 3][j - 3]
-            if palavra == 'XMAS' or palavra == 'SAMX':
-                cont_diagonal_esquerda += 1
+            if i < linhas - 3:
+                teste = arquivo[i][j] + arquivo[i+1][j] + arquivo[i+2][j] + arquivo[i+3][j]
+                cont += 1 if teste in palavras else 0
 
-    return cont_horizontal + cont_vertical + cont_diagonal_direita + cont_diagonal_esquerda
+            if i < linhas - 3 and j < colunas - 3:
+                teste =  arquivo[i][j] + arquivo[i+1][j+1] + arquivo[i+2][j+2] + arquivo[i+3][j+3]
+                cont += 1 if teste in palavras else 0
+
+            if i < linhas - 3 and j > 2:
+                teste =  arquivo[i][j] + arquivo[i+1][j-1] + arquivo[i+2][j-2] + arquivo[i+3][j-3]
+                cont += 1 if teste in palavras else 0
+
+    return cont
 
 
-def part_2(arquivo: str) -> int:
+def part_2(arquivo: list[str]) -> int:
     cont_x_mas = 0
+    palavras = ['MAS', 'SAM']
 
     for i in range(1, len(arquivo) - 1):
         for j in range(1, len(arquivo[i]) - 1):
             if arquivo[i][j] == 'A':
                 diagonal_direita = arquivo[i - 1][j - 1] + arquivo[i][j] + arquivo[i + 1][j + 1]
                 diagonal_esquerda = arquivo[i + 1][j - 1] + arquivo[i][j] + arquivo[i - 1][j + 1]
-                if diagonal_direita in ['MAS', 'SAM'] and diagonal_esquerda in ['MAS', 'SAM']:
+                if diagonal_direita in palavras and diagonal_esquerda in palavras:
                     cont_x_mas += 1
 
     return cont_x_mas
