c751a698227eed5e035be73301493e4a276ea277
Pablech <padagoec@gmail.com>
2025-12-27 18:45:14 -0300

    Atualização do day 12 parte 1

--- day_12/day_12_Garden-Groups.py
+++ day_12/day_12_Garden-Groups.py
@@ -1,15 +1,29 @@
 VISITADOS = set()
-CONTADOR = 0
 
 
-def mapear(mapa: list[list[str]], inicio: tuple[int, int], alvo: str, plantas: int = 0):
-    global VISITADOS, CONTADOR
+def perimetro_e_area(grupo: set[tuple[int, int]]) -> tuple[int, int]:
+    perimetro = 0
+
+    for i, j in grupo:
+        for di, dj in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+            ni, nj = i + di, j + dj
+            if (ni, nj) not in grupo:
+                perimetro += 1
+
+    return perimetro, len(grupo)
+
+
+def mapear(mapa: list[list[str]], inicio: tuple[int, int], alvo: str, grupo_param: set[tuple[int, int]]) -> tuple[
+    int, int]:
+    global VISITADOS
 
     altura = len(mapa)
     largura = len(mapa[0])
 
     i, j = inicio
-    testes = []
+    testes = [inicio]
+
+    grupo = set() if not grupo_param else grupo_param
 
     if i < altura - 1:
         baixo = (i + 1, j)
@@ -26,29 +40,31 @@ def mapear(mapa: list[list[str]], inicio: tuple[int, int], alvo: str, plantas: i
     if j > 0:
         esquerda = (i, j - 1)
         testes.append(esquerda)
+
     for t in testes:
         i, j = t
         if (i, j) not in VISITADOS and mapa[i][j] == alvo:
             VISITADOS.add((i, j))
-            CONTADOR += 1
-            print(f'{mapa[i][j] = } - {CONTADOR = } - {i} {j}')
-            mapear(mapa, (i, j), alvo)
+            grupo.add((i, j))
+            mapear(mapa, (i, j), alvo, grupo)
 
-
+    return perimetro_e_area(grupo)
 
 
 def main():
-    global  VISITADOS
+    global VISITADOS
 
     with open('input.txt', 'r', encoding='utf-8') as f:
         mapa = [list(linha) for linha in f.read().splitlines()]
 
-
+    preco = 0
     for i in range(len(mapa)):
         for j in range(len(mapa[i])):
             if (i, j) not in VISITADOS:
-                # VISITADOS.add((i, j))
-                mapear(mapa, (i,j), mapa[i][j])
+                perimetro, area = mapear(mapa, (i, j), mapa[i][j], set())
+                preco += perimetro * area
+
+    print(f'Total part 1: {preco}')
 
 
 if __name__ == '__main__':
